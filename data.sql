6ad9933106e39a758f3d37064467e9ad3198d718
Muraliece88 <33730338+Muraliece88@users.noreply.github.com>
2024-08-14 20:38:11 +0200

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Product (name,brand,quantity,curency,price,seller) VALUES ('iPhone15','Apple','4','USD','1300','Amazon');
+INSERT INTO Product (name,brand,quantity,curency,price,seller) VALUES ('S25','Samsung','4','USD','1500','Bol');
